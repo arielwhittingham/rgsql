900362d80fe80e1c8fdc764a0ab3ad700f54034b
Ariel <99064413+arielwhittingham@users.noreply.github.com>
2025-12-21 16:00:49 -0500

    making a server process

---
+++ scratch/scratch_server_process.py
@@ -0,0 +1,12 @@
+# https://www.datacamp.com/tutorial/a-complete-guide-to-socket-programming-in-python
+
+import socket
+
+_SERVER_IP = "127.0.0.1"
+_PORT = 8000
+
+def run_server():
+    # server code
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((_SERVER_IP, _PORT))
+
